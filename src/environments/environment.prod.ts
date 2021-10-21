01e01689bbd4053d3f5a1103a60d3124c93b799c
koby5i <bobkoby5i@xdeq.com>
2021-10-21 23:59:10 +0200

    HEROKU FE configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlBe: 'https://koby5i-intro-rent-a-car-be.herokuapp.com',
+  baseUrlFe: 'https://koby5i-intro-rent-a-car.herokuapp.com'
 };

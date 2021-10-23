e9fc21ced58d3609efb9dad98e30914e06221fc3
koby5i <bobkoby5i@xdeq.com>
2021-10-24 01:50:09 +0200

    30 Finish register BE: bcrypt, mode-user, mongoose, jsonwebtoken

--- backend/user.js
+++ backend/user.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router();
+
+const User = require('./models/model-user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+
  
 router.post('/register', (req, res, next) => {
-    console.log('POST /api/user/register received in user.js')
+    console.log('POST /api/user/register - received in user.js' );
+    console.log('user: ' + req.body.email);
+    bcrypt.hash(req.body.password, 10).then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash, 
+            isAdmin: 0 
+        });
+        user.save().then(result => {
+            res.status(200).json({message:'User Created'});
+        }).catch(err => {
+            console.log(err);
+        })
+    })
     //res.send('Hello from Server GET /api/user/register')
-    res.status(201).json({message: 'User register SUCCESS. Response from nodeJs as json'})
-    //res.send.json({message: 'Response from nodeJs as json'})
+    res.status(201).json({message: 'User register SUCCESS. Response from nodeJs as json'});
 })
  
 module.exports = router;

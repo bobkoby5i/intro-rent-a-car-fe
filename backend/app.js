775e05b3cb3d3ac7675c0600e2eb57f3d5d8ea5d
koby5i <bobkoby5i@xdeq.com>
2021-10-24 00:52:46 +0200

    27 add cors and body-parser to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 const express = require('express')
-const router = express.Router()
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const app = express.Router()
 //const appbe = express();
 
-router.get('/hello', function(req, res){
+app.use(cors())
+app.use(bodyparser.json())
+
+app.get('/hello', function(req, res){
     console.log('GET /api/hello from app.js');
     res.send('GET /api/hello from app.js')
 })
 
 
-router.use((req, res,next) => {
+app.use((req, res,next) => {
     console.log('GET * This a response from app.js');
     res.send('GET * This a response from app.js')
 });
 
 
-module.exports = router;
+module.exports = app;
 
--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const bodyParser = require('body-parser')
 
 
 const port = process.env.PORT || 3000;
-const app = express()
-app.use(express.json());
-//app.use(bodyParser.json())
-app.use(express.urlencoded({extended: false}));
+const server = express()
+server.use(express.json());
+//server.use(bodyParser.json())
+server.use(express.urlencoded({extended: false}));
 
-const apiapp = require('./backend/api-app'); // refers to ./backedn/app.js
-app.use('/api', apiapp);
-app.use(express.static('rent-a-car-fe/dist'))
-app.get('/hello', function(req, res){
+const app = require('./backend/app'); // refers to ./backedn/app.js
+server.use('/api', app);
+server.use(express.static('rent-a-car-fe/dist'))
+server.get('/hello', function(req, res){
     console.log('GET /hello');    
     res.send('Hello from Server')
 })
@@ -23,7 +23,7 @@ console.log('Starting server on localhost:' + port)
 // app.listen(port, function(){
 //     console.log('Server running on localhost:' + port)
 // })
-app.listen(port, console.log('Server running on localhost:' + port))
+server.listen(port, console.log('Server running on localhost:' + port))
 
 
  

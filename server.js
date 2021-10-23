61145cc01fedc942b06599e1eee51cf73e4a3013
koby5i <bobkoby5i@xdeq.com>
2021-10-23 23:30:45 +0200

    21 nodeJS BE init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const bodyParser = require('body-parser')
+ 
+const PORT = 3000
+const app = express()
+ 
+app.use(bodyParser.json())
+ 
+app.get('/', function(req, res){
+    res.send('Hello from Server')
+})
+ 
+app.listen(PORT, function(){
+    console.log('Server running on localhost:' + PORT)
+})

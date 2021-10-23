03f19706e355ef58ce87f3c8e0d83450b4e73702
koby5i <bobkoby5i@xdeq.com>
2021-10-24 01:59:31 +0200

    31.2 mock user, bcrypt, jwt token

--- backend/user.js
+++ backend/user.js
@@ -30,22 +30,43 @@ router.post('/register', (req, res, next) => {
 router.post('/login', (req, res, next) => {
     console.log('POST /api/user/login - received in user.js' );
     console.log('user: ' + req.body.email);
-    p_email = req.body.email
+    let fetcheduser;
+    const p_email = req.body.email
+    const p_pass = req.body.password
+    //User.findOne({email: p_email}).then(user => {
     bcrypt.hash(req.body.password, 10).then(hash => {
         console.log('pass: ' + hash);
         const user = new User({
+            _id: "1234567890",
             email: "a@a.com",
-            password: hash, 
-            isAdmin: 0 
+            //password = "password"
+            password: "$2a$10$8ckbvK6KPK.JP1EVwEDsZugWo1YJMGr.HHDvWkEOlHI6CFHLPaI9W",
+            isAdmin: 0
         });
-        if (user.email != p_email) {
-            console.log('email does not match a@a.com - sorry');
-            return res.status(401).json({message:"Auth failed."})
+
+        if (!user) {
+            console.log('Auth failed: User not found.');
+            return res.status(401).json({message:"Auth failed: User not found."}) 
         }
-        console.log('email match a@a.com - ok');
-        res.status(200).json({message: 'User login SUCCESS. enjoy'})
-    })
-    //res.send('Hello from Server GET /api/user/register')
+        fetcheduser = user;
+        console.log('user found. checking passwd');
+        return bcrypt.compare(p_pass, user.password)
+    }).then(result => {
+        if (!result){
+            console.log('Auth failed: Password does not match.');
+            return res.status(401).json({message:"Auth failed: Password does not match."}) 
+        }
+        console.log('user found. passwd OK. generate token');        
+        const p_sign_secret = "jwt_secret"
+        const p_isAdmin = fetcheduser.isAdmin;
+        const p_token = jwt.sign({email: fetcheduser.email, userId: fetcheduser.isAdmin._id},p_sign_secret, {expiresIn:'1h'});
+        const p_expires = 3600
+        console.log('token:'+p_token);   
+        return res.status(200).json({token:p_token, expiresIn: p_expires, admin:p_isAdmin}) 
+
+    }).catch(err =>{
+        console.log(err);
+    });
     
 });
  

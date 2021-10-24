412bd5951865105d078fcf62e50764263d3c3817
koby5i <bobkoby5i@xdeq.com>
2021-10-24 14:37:12 +0200

    33 Connection to MongoDb Atlas

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,25 @@ const express = require('express')
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const app = express.Router()
+const mongoose = require('mongoose')
+// SET "MONGO_RENT_A_CAR_URI=mongodb+srv://intro-rent-a-car-user:<password>@<dbname>.tlx1x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongo_uri = process.env.MONGO_RENT_A_CAR_URI || "mongodb://localhost:27017/rent-a-car";
 //const appbe = express();
+
+
+// mongoose.connect(mongo_uri, err=> {
+//     if (err) {
+//         console.error('Error' + err)
+//     } else {
+//         console.log('Connected to mongodb')
+//     }
+// })
+
+mongoose.connect(mongo_uri, {useNewUrlParser: true}).then(() => {
+    console.log('Connected to mongodb')
+}).catch(err => console.log(err))
+
+
 
 app.use(cors())
 app.use(bodyparser.json())
@@ -17,9 +35,11 @@ app.get('/',(req, res,next) => {
     res.send('This a response from app.js')
 });
 
-
 const userRoutes = require('./user'); //reference to ./user.js
 app.use('/user', userRoutes)
 
+
 module.exports = app;
+
+
 

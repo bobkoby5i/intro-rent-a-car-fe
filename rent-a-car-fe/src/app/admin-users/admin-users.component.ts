bd039369d3803ec5f071d7ee6d9073f367efdebf
koby5i <bobkoby5i@xdeq.com>
2021-10-26 20:30:27 +0200

    41 admin-users.component Put Data to our Mat Table from Backedn

--- rent-a-car-fe/src/app/admin-users/admin-users.component.ts
+++ rent-a-car-fe/src/app/admin-users/admin-users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { AdminService } from '../services/admin.service';
 
 @Component({
@@ -10,10 +10,12 @@ import { AdminService } from '../services/admin.service';
 })
 export class AdminUsersComponent implements OnInit {
 
-  displayedColumns: string[] = [ 'email', 'isAdmin'];
-  dataSource = new MatTableDataSource<Users>();
+  displayedColumns: string[] = [ 'email', 'isAdmin', 'edit'];
+  dataSource = new MatTableDataSource<User>();
+  users: any;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild('table') table: MatTable<any>;
 
 
   constructor(private adminservice: AdminService) { }
@@ -21,7 +23,21 @@ export class AdminUsersComponent implements OnInit {
   ngOnInit(): void {
     this.adminservice.getUsers().subscribe(res => {
       console.log(res);
+      const ELEMENT_DATA: UserRow[] = [];
+      this.users = res;
+      this.users.forEach((element: User) => {
+        const user_row: UserRow = {
+          email:   element.email,
+          isAdmin: element.isAdmin,
+          action: 'EDIT'
+        }
+        //const email = element.email;
+        //const isAdmin = element.email;
+        //const action = "ACTION";
+        ELEMENT_DATA.push(user_row);
+      });
       this.dataSource.data = ELEMENT_DATA;
+      console.log(ELEMENT_DATA);
     });
     
   }
@@ -32,14 +48,19 @@ export class AdminUsersComponent implements OnInit {
   
 }
 
-export interface Users {
+export interface UserRow {
+  email: string;
+  isAdmin: number;
+  action: string;
+}
+export interface User {
   email: string;
   isAdmin: number;
 }
 
 
 
-const ELEMENT_DATA: Users[] = [
+const ELEMENT_DATA2: User[] = [
   {email: 'Hydrogen', isAdmin: 0},
   {email: 'Helium', isAdmin: 0},
   {email: 'Lithium', isAdmin: 0},

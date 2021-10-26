1d739c947e78a1a026caa8ab96c964f9658c0b45
koby5i <bobkoby5i@xdeq.com>
2021-10-26 20:09:31 +0200

    40 admin-users.component table with ELEMENT_DATA mock data

--- rent-a-car-fe/src/app/admin-users/admin-users.component.ts
+++ rent-a-car-fe/src/app/admin-users/admin-users.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { AdminService } from '../services/admin.service';
 
 @Component({
@@ -8,10 +10,41 @@ import { AdminService } from '../services/admin.service';
 })
 export class AdminUsersComponent implements OnInit {
 
+  displayedColumns: string[] = [ 'email', 'isAdmin'];
+  dataSource = new MatTableDataSource<Users>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
   constructor(private adminservice: AdminService) { }
 
   ngOnInit(): void {
-    this.adminservice.getUsers().subscribe(res => console.log(res));
+    this.adminservice.getUsers().subscribe(res => {
+      console.log(res);
+      this.dataSource.data = ELEMENT_DATA;
+    });
+    
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  
 }
+
+export interface Users {
+  email: string;
+  isAdmin: number;
+}
+
+
+
+const ELEMENT_DATA: Users[] = [
+  {email: 'Hydrogen', isAdmin: 0},
+  {email: 'Helium', isAdmin: 0},
+  {email: 'Lithium', isAdmin: 0},
+  {email: 'Beryllium', isAdmin: 0},
+  {email: 'Boron', isAdmin: 0},
+  {email: 'Carbon', isAdmin: 0},
+  {email: 'Nitrogen', isAdmin: 0},
+];

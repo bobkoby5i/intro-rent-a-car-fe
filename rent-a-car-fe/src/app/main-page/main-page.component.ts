9b0c101e456853d2f813e1657845f812bf3ab848
koby5i <bobkoby5i@xdeq.com>
2021-10-28 22:49:46 +0200

    51 main-page get cars from user.service using selectedCars Subject()

--- rent-a-car-fe/src/app/main-page/main-page.component.ts
+++ rent-a-car-fe/src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  cars:any;
+
+  constructor(private userservice: UserService) { }
 
   ngOnInit(): void {
+    this.userservice.selectedCars.subscribe(res => {
+      this.cars = res;
+    });
+
   }
 
 }

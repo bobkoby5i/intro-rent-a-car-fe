8e244636fe41237ebdcd7a5eeed00b5a083c6671
koby5i <bobkoby5i@xdeq.com>
2021-11-01 22:11:49 +0100

    fix register response

--- backend/user.js
+++ backend/user.js
@@ -6,14 +6,105 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
 
- 
-router.post('/register', (req, res, next) => {
+// added async to wait for bcrypt. otherwise then needed like before.
+// this is option with await. 
+// and genarate token
+router.post('/register', async (req, res, next) => {
+    console.log('POST /api/user/register - received in user.js' );
+    console.log('user: ' + req.body.email);
+
+    const hash = await bcrypt.hash(req.body.password, 10);
+    console.log('hash: ' + hash);
+    const newUser = new User({
+        email: req.body.email,
+        password: hash,
+        isAdmin: 0
+    });       
+
+    User.findOne({email: req.body.email}).then(registeredUser => {
+        if (registeredUser) {
+            console.log('Register user failed. User already exists !')  
+            res.status(409).json({message: 'Register user failed. User already exists !'});
+        } else {
+            console.log('saving user in mongo...');
+            newUser.save().then(result => {
+                console.log('Save completed.');
+                let token = jwt.sign(payload, 'my_encryption_password')
+                res.status(200).send({token}) 
+                res.status(201).json({message:'Register new user - SUCCESS'});
+            }).catch(err => { console.log(err);}) // save error catch
+        }
+    }).catch(err => { console.log(err);}) // find one error catch
+    console.log('Register user() finished');
+})
+
+
+// added async to wait for bcrypt. otherwise then needed like before.
+// this is option with await. 
+router.post('/register2', async (req, res, next) => {
+    console.log('POST /api/user/register - received in user.js' );
+    console.log('user: ' + req.body.email);
+
+    const hash = await bcrypt.hash(req.body.password, 10);
+    console.log('hash: ' + hash);
+    const newUser = new User({
+        email: req.body.email,
+        password: hash,
+        isAdmin: 0
+    });       
+
+    User.findOne({email: req.body.email}).then(registeredUser => {
+        if (registeredUser) {
+            console.log('Register user failed. User already exists !')  
+            res.status(409).json({message: 'Register user failed. User already exists !'});
+        } else {
+            console.log('saving user in mongo...');
+            newUser.save().then(result => {
+                console.log('Save completed.');
+                res.status(201).json({message:'Register new user - SUCCESS'});
+            }).catch(err => { console.log(err);}) // save error catch
+        }
+    }).catch(err => { console.log(err);}) // find one error catch
+    console.log('Register user() finished');
+})
+
+
+// this is second better option 
+router.post('/register2', (req, res, next) => {
+    console.log('POST /api/user/register - received in user.js' );
+    console.log('user: ' + req.body.email);
+
+    User.findOne({email: req.body.email}).then(registeredUser => {
+        if(registeredUser) {
+            console.log('Register user failed. User already exists !')  
+            res.status(400).json({message: 'Register failed. User already exiss.'});
+        } else {
+            bcrypt.hash(req.body.password, 10).then(hash =>{
+                const user = new User({
+                    email: req.body.email,
+                    password: hash,
+                    isAdmin: 0
+                });   
+                console.log('saving user in mongo...');
+                user.save().then(result => {
+                    console.log('saved');
+                    res.status(201).json({message:'User register SUCCESS. Response from nodeJs as json'});
+                }).catch(err => {console.log(err)})
+            })
+        } 
+    }).catch(err => { console.log(err);}) // find one error catch
+})
+
+
+
+// I think this is bad 2021-11-01
+router.post('/register1_old', (req, res, next) => {
     console.log('POST /api/user/register - received in user.js' );
     console.log('user: ' + req.body.email);
 
     User.findOne({email: req.body.email}).then(user => {
         fetcheduser = user;
-        if(user) {
+        if(fetcheduser) {
           return res.status(400).json({message: 'Register failed. User already exiss.'});
         }
         return bcrypt.hash(req.body.password, 10)
@@ -44,16 +135,7 @@ router.post('/login', (req, res, next) => {
     const p_email = req.body.email
     const p_pass = req.body.password
     User.findOne({email: p_email}).then(user => {
-    //bcrypt.hash(req.body.password, 10).then(hash => {
         console.log('email: ' + p_email);
-        // const user = new User({
-        //     _id: "1234567890",
-        //     email: "a@a.com",
-        //     //password = "password"
-        //     password: "$2a$10$8ckbvK6KPK.JP1EVwEDsZugWo1YJMGr.HHDvWkEOlHI6CFHLPaI9W",
-        //     isAdmin: 0
-        // });
-
         if (!user) {
             console.log('Auth failed: User not found.');
             return res.status(401).json({message:"Auth failed: User not found."}) 

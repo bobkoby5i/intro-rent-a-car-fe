784832dd7d8d640c6ed250916d169147cea124ca
koby5i <bobkoby5i@xdeq.com>
2021-11-01 23:00:25 +0100

    C:/tools/git/register also returns jwt token

--- backend/user.js
+++ backend/user.js
@@ -5,6 +5,9 @@ const User = require('./models/model-user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
+const JWT_ENCRIPTION_PASSWORD = 'my_encryption_password'
+const JWT_EXPIRES_IN          = 3600
+
 
 // added async to wait for bcrypt. otherwise then needed like before.
 // this is option with await. 
@@ -16,8 +19,9 @@ router.post('/register', async (req, res, next) => {
     const hash = await bcrypt.hash(req.body.password, 10);
     console.log('hash: ' + hash);
     const newUser = new User({
-        email: req.body.email,
-        password: hash,
+        email    : req.body.email,
+        isAdmin  : 0,
+        password : hash,
     });       
 
     User.findOne({email: req.body.email}).then(registeredUser => {
@@ -26,9 +30,27 @@ router.post('/register', async (req, res, next) => {
             res.status(409).json({message: 'Register user failed. User already exists !'});
         } else {
             console.log('saving user in mongo...');
-            newUser.save().then(result => {
+            newUser.save().then(savedUser => {
                 console.log('Save completed.');
-                res.status(201).json({message:'Register new user - SUCCESS'});
+                let isAdmin = 0;
+                let payload = {                                         // prepare payload
+                    subject    : savedUser._id,
+                    userId     : savedUser._id,
+                    email      : savedUser.email,
+                    isAdmin    : isAdmin
+
+                }           
+                const token = jwt.sign(payload,JWT_ENCRIPTION_PASSWORD, {expiresIn:JWT_EXPIRES_IN}); // symetric encription
+                console.log('token: ' + token);   
+                //res.status(200).send({token})    
+                res.status(200).json({
+                    token      : token, 
+                    expiresIn  : JWT_EXPIRES_IN, 
+                    email      : savedUser.email,                
+                    isAdmin    : isAdmin
+                }) 
+                
+                //res.status(201).json({message:'Register new user - SUCCESS'});
             }).catch(err => { console.log(err);}) // save error catch
         }
     }).catch(err => { console.log(err);}) // find one error catch
@@ -38,7 +60,7 @@ router.post('/register', async (req, res, next) => {
 
 // added async to wait for bcrypt. otherwise then needed like before.
 // this is option with await. 
-router.post('/register2', async (req, res, next) => {
+router.post('/register3', async (req, res, next) => {
     console.log('POST /api/user/register - received in user.js' );
     console.log('user: ' + req.body.email);
 
@@ -94,7 +116,7 @@ router.post('/register2', (req, res, next) => {
 
 
 
-// I think this is bad 2021-11-01
+// I think this is bad 2021-11-01 was written by me based on login using return 
 router.post('/register1_old', (req, res, next) => {
     console.log('POST /api/user/register - received in user.js' );
     console.log('user: ' + req.body.email);
@@ -125,7 +147,7 @@ router.post('/register1_old', (req, res, next) => {
 })
 
 
-router.post('/login', (req, res, next) => {
+router.post('/login1', (req, res, next) => {
     console.log('POST /api/user/login - received in user.js' );
     console.log('user: ' + req.body.email);
     let fetcheduser;
@@ -145,19 +167,68 @@ router.post('/login', (req, res, next) => {
             console.log('Auth failed: Password does not match.');
             return res.status(401).json({message:"Auth failed: Password does not match."}) 
         }
-        console.log('user found. passwd OK. generate token');        
-        const p_sign_secret = "jwt_secret"
-        const p_isAdmin = fetcheduser.isAdmin;
-        const p_token = jwt.sign({email: fetcheduser.email, userId: fetcheduser._id, isAdmin: p_isAdmin},p_sign_secret, {expiresIn:'1h'});
-        const p_expires = 3600
-        console.log('token:'+p_token);   
-        return res.status(200).json({token:p_token, expiresIn: p_expires, admin:p_isAdmin}) 
+        console.log('user found. passwd OK. generate token...');        
+        let payload = {                                         // prepare payload
+            subject    : fetcheduser._id,
+            userId     : fetcheduser._id,
+            email      : fetcheduser.email,
+            isAdmin    : fetcheduser.isAdmin
+        }           
+        const token = jwt.sign(payload,JWT_ENCRIPTION_PASSWORD, {expiresIn:JWT_EXPIRES_IN}); // symetric encription
+        console.log('token: ' + token);   
+        //res.status(200).send({token})    
+        res.status(200).json({
+            token      : token, 
+            email      : fetcheduser.email,            
+            expiresIn  : JWT_EXPIRES_IN, 
+            isAdmin    : fetcheduser.isAdmin
+        }) 
 
     }).catch(err =>{
         console.log(err);
     });
-    
 });
+
+router.post('/login', (req, res, next) => {
+    console.log('POST /api/user/login - received in user.js' );
+    console.log('user: ' + req.body.email);
+    let fetcheduser;
+    const p_email = req.body.email
+    const p_pass = req.body.password
+    User.findOne({email: p_email}).then(user => {
+        console.log('email: ' + p_email);
+        if (!user) {
+            console.log('Auth failed: User not found.');
+            return res.status(401).json({message:"Auth failed: User not found."}) 
+        } 
+        fetcheduser = user;
+        console.log('user found. checking passwd');
+        bcrypt.compare(p_pass, user.password).then(result =>{
+            if (!result){
+                console.log('Auth failed: Password does not match.');
+                return res.status(401).json({message:"Auth failed: Password does not match."}) 
+            }
+            console.log('user found. passwd OK. generate token...');        
+            const payload = {                                         // prepare payload
+                subject    : fetcheduser._id,
+                userId     : fetcheduser._id,
+                email      : fetcheduser.email,
+                isAdmin    : fetcheduser.isAdmin
+            }           
+            const token = jwt.sign(payload, JWT_ENCRIPTION_PASSWORD, {expiresIn:JWT_EXPIRES_IN}); // symetric encription
+            console.log('token: ' + token);
+
+            const response = {
+                token      : token, 
+                expiresIn  : JWT_EXPIRES_IN, 
+                email      : fetcheduser.email,                
+                isAdmin    : fetcheduser.isAdmin
+            }
+            res.status(200).json(response) 
+        }).catch(err =>{console.log(err)}); // bcrypt() error catch
+    }).catch(err =>{console.log(err)}); // findOne() error catch
+});
+ 
  
  
 module.exports = router;
